22bbf1f16080a476df32b211a1f8f806904a2214
beingsoubhik <bipman2002@gmail.com>
2025-10-08 19:24:56 +0530

    Add files via upload

---
+++ simple python2.py
@@ -0,0 +1,12 @@
+a = input('Enter a number: ')
+b = input('Enter another number: ')
+a = int(a)
+b = int(b)
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+print('Addition : ',c)
+print('Subtraction : ',d)
+print('Multiplication: ',e)
+print('Division: ',f)
