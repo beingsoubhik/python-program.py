8fd4c3d9d4da584eddcab69c7ae856ad838c553f
beingsoubhik <bipman2002@gmail.com>
2025-11-13 14:17:01 +0530

    Delete math_practice.py

--- math_practice.py
+++
@@ -1,8 +0,0 @@
-import math
-num = int(input("Enter a number: "))
-b = math.sqrt(num)
-c= math.log(num)
-d = math.sin(num)
-print(f"Square root: {b}")
-print(f"Logarithm : {c}")
-print(f"Sine: {d}")
